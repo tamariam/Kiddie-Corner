08af403354ef3a8c4b651e56a5756690956d47c8
sopiko abulidze <abulidze2019@gmail.com>
2024-04-22 20:42:37 +0000

    create products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Category(models.Model):
+
+    """
+    models for product categories
+
+    """
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    in_stock = models.BooleanField(
+        default=False, null=True, blank=True)
+    favourite = models.ManyToManyField(
+        User, related_name='favourite', blank=True)
+    description = models.TextField(max_length=1024, default="")
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    featured_image = CloudinaryField("Product Image", default="placeholder")
+    rating = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True )
+
+    def __str__(self):
+        return self.name
+
+
