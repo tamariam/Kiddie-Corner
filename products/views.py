c9e1e29d7e116ad08b46c2476b5df57b766d44ab
sopiko abulidze <abulidze2019@gmail.com>
2024-04-25 13:40:44 +0000

    create basic view for product detail, import 404 object

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.shortcuts import get_object_or_404
 
 def all_products(request):
     products = Product.objects.all()
@@ -10,3 +10,13 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html',  context)
+
+
+def product_detail(request, product_id):
+    product = get_objetc_or_404(Product, pk=product_id)
+
+    context = {
+      'product':product,
+    }
+
+    return render(request, 'products/product_detail.html',  context)
