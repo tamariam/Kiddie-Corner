9ffb328f0be3250d15433f3f3efa8742597b545c
sopiko abulidze <abulidze2019@gmail.com>
2024-04-29 16:01:16 +0000

    modify all_products view to dynamically filter products by category

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from .models import Product
+from django.shortcuts import render, redirect, reverse
+from .models import Product,Category
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
@@ -8,19 +8,33 @@ from django.db.models import Q
 def all_products(request):
     products = Product.objects.all()
     query = None
+    category_filter = None  # Initialize category filter variable
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, 'Enter what you are looking for?')
-                return redirect('products:products')
+                messages.error(request, 'Please enter what you are looking for.')
+                return redirect(reverse('products'))
 
+            # Define filter criteria for search query
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+        if 'category' in request.GET:
+            category_filter = request.GET['category']
+            if category_filter:  # Ensure category filter is not empty
+                # Filter products by category
+                products = products.filter(category__name=category_filter)
+
+    # Retrieve all categories for rendering in template
+    categories = Category.objects.all()
+
     context = {
         'products': products,
+        'categories': categories,  # Pass all categories to template
+        'query': query,
+        'category_filter': category_filter,  # Pass category filter value to template
     }
 
     return render(request, 'products/products.html', context)
