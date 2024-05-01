d0392010758689d0649c68561a3606a6a05a4cef
sopiko abulidze <abulidze2019@gmail.com>
2024-05-01 13:48:19 +0000

    Implement 'Update context.py to initialize 'bag' variable from sessios,Implement 'add_to_bag' view and define URL patterns for adding products to the shopping bag

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def shopping_bag_contents(request):
     bag_items = []
     total = 0
     count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_SHIPPING_LIMIT:
         shipping = total * Decimal(settings.STANDARD_SHIPPING_PECRENT/100)
--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import shopping_bag
+from .views import shopping_bag, add_to_bag
 
 urlpatterns = [
     path('', shopping_bag, name='shopping_bag'),
+    path('', add_to_bag, name='add_to_bag'),
+    path('add/<item_id>/', add_to_bag, name='add_to_bag'),
 ]
