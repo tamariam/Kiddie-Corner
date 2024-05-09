12baa60b11a2352982c3a5265611fcabf3aa5000
sopiko abulidze <abulidze2019@gmail.com>
2024-05-09 16:04:11 +0000

    register Order model in admin panel, define fielsd for readonly, display, search and filter

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    register  the Order model in Admin panel
+    """
+    fields = (
+        'order_number', 'full_name', 'date',
+        'email', 'phone_number', 'country',
+        'postcode', 'town_or_city', 'street_address1',
+        'street_address2', 'county', 'shipping_cost',
+        'order_total', 'grand_total', 'original_bag',
+        'order_status', 'shipping_method',
+    )
+    list_display = (
+        'full_name', 'email', 'phone_number', 'order_number',
+        'date', 'shipping_cost',
+        'order_status', 'order_total', 'grand_total'
+        )
+    search_fields = (
+        'full_name', 'phone_number',
+        'email', 'order_number',)
+
+    readonly_fields = (
+        'order_number', 'order_total', 'grand_total'
+        'date', 'shipping_cost', 'original_bag'
+
+    )
+
+    list_filter = (
+        'order_number',  'full_name',
+        'email', 'phone_number', 'date'
+    )
+    
+    ordering = ('-date',)
