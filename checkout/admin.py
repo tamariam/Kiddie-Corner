49b8439252634a80a681ecd7ec2ee6dbcab9cd96
sopiko abulidze <abulidze2019@gmail.com>
2024-05-09 16:13:16 +0000

    Add OrderLineItem as an inline on the admin panel for better visibility and management of order details

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,11 +4,17 @@ from .models import Order, OrderLineItem
 # Register your models here.
 
 
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """
     register  the Order model in Admin panel
     """
+    inlines = (OrderLineItemAdminInline)
     fields = (
         'order_number', 'full_name', 'date',
         'email', 'phone_number', 'country',
@@ -20,7 +26,7 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = (
         'full_name', 'email', 'phone_number', 'order_number',
         'date', 'shipping_cost',
-        'order_status', 'order_total', 'grand_total'
+        'order_status', 'order_total', 'grand_total',
         )
     search_fields = (
         'full_name', 'phone_number',
@@ -28,13 +34,12 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = (
         'order_number', 'order_total', 'grand_total'
-        'date', 'shipping_cost', 'original_bag'
+        'date', 'shipping_cost', 'original_bag',
 
     )
 
     list_filter = (
-        'order_number',  'full_name',
-        'email', 'phone_number', 'date'
+        'order_number',  'full_name', 'shipping_method', 'date'
     )
     
     ordering = ('-date',)
