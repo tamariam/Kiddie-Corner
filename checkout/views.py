6000825ba12539b188a48b32ebb3a40fec95d163
sopiko abulidze <abulidze2019@gmail.com>
2024-05-13 11:48:35 +0000

    Add basic checkout view to handle checkout process; render checkout page with checkout form

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import CheckoutForm
 
 # Create your views here.
 
 
 def checkout(request):
-    return render(request, 'checkout/checkout.html')
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Your shopping bag is  empty")
+        return redirect(reverse('products'))
+    checkout_form = CheckoutForm()
+    template = 'checkout/checkout.html'
+    return render(request, template, {'checkout_form': checkout_form})
