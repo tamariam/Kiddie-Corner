5fd7a7704f843dfea124fb74082903d04e15e4b4
sopiko abulidze <abulidze2019@gmail.com>
2024-05-14 15:56:56 +0000

    Add JavaScript error handling for Stripe card input validation

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,6 @@
 const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 const client_secret = $('#id_client_secret').text().slice(1, -1);
+const errorDiv = $("#error-message");
 
 const stripe = Stripe(stripe_public_key);
 
@@ -22,3 +23,27 @@ let style = {
 
 let card = elements.create('card', {'style':style})
 card.mount('#payment-element');
+
+//validation errors on card payment
+let getErrorMessage=(event)=>{
+    return `
+    <span role="alert">
+    <i class="fa-solid fa-triangle-exclamation fa-sm"></i>
+    </span>
+    <span>${event.error.message}</span>
+    `;
+
+}
+
+card.on('change', (event)=>{
+    if (event.error){
+    let errorText=getErrorMessage(event)
+    $(errorDiv).html(errorText);
+    }else {
+        errorDiv.textContent = '';
+    }
+})
+
+
+
+
