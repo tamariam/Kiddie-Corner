4cc20b6b736d97d9f136666a0a108846b3673490
sopiko abulidze <abulidze2019@gmail.com>
2024-05-14 13:35:50 +0000

    start working tish stripe payment , include stripe link on  base html file

--- checkout/views.py
+++ checkout/views.py
@@ -12,4 +12,8 @@ def checkout(request):
         return redirect(reverse('products'))
     checkout_form = CheckoutForm()
     template = 'checkout/checkout.html'
-    return render(request, template, {'checkout_form': checkout_form})
+    context = {
+        'stripe_public_key': 'pk_test_51OzJsR09cBHepEM8cwk9sLwFJXb08KhYt32EiXPfiumpqcY83g0K2nlMoKTtwdVgtx5llQUZmmjaTXnUCHKLchfB00ZtCNCDQc',
+        'client_secret': 'client_Secret',
+    }
+    return render(request, template, context, {'checkout_form': checkout_form})
