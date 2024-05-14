3e0c5903244527d96f69b5e00768e515d82de4bf
sopiko abulidze <abulidze2019@gmail.com>
2024-05-14 20:54:41 +0000

    add stripe publi and secret key variables to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -9,18 +9,20 @@ from django.conf import settings
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your shopping bag is  empty, please choose items to purchase")
         return redirect(reverse('products'))
-    current_bag=shopping_bag_contents(request)
-    total=current_bag['grand_total']
-    stripe_total=round(total*100)
+    current_bag = shopping_bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total*100)
     checkout_form = CheckoutForm()
     template = 'checkout/checkout.html'
     context = {
         'checkout_form': checkout_form,
-        'stripe_public_key': 'pk_test_51OzJsR09cBHepEM8cwk9sLwFJXb08KhYt32EiXPfiumpqcY83g0K2nlMoKTtwdVgtx5llQUZmmjaTXnUCHKLchfB00ZtCNCDQc',
+        'stripe_public_key': stripe_public_key,
         'client_secret': 'client_Secret',
     }
     return render(request, template, context)
