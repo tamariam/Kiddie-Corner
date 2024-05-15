56de6c059980b0a5d4e6287a845fddf20f2831cf
sopiko abulidze <abulidze2019@gmail.com>
2024-05-15 13:30:42 +0000

    copy checkout html template for future modifications

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import checkout,checkout_success
+from .views import checkout, checkout_success
 
 urlpatterns = [
     path('', checkout, name='checkout'),
