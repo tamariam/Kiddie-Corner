f1e164a8646dd9c33223fce21c902d8ad6e823ce
sopiko abulidze <abulidze2019@gmail.com>
2024-05-16 19:51:09 +0000

    Refactor handle_payment_intent_succeeded to ensure order creation if missing in databasese

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -41,12 +41,57 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        order_exists = False
+        try:
+            order = Order.object.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    eircode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                message = f'Webhook received: {event["type"]}|SUCCESS:Verified order already in database'
+                return HttpResponse(
+                content=message, status=200
+                )
+        except Order.DoesNotExist:
+            try:
+             
+                order = Order.objects.create(
+                full_name=shipping_details.name,
+                email=billing_details.email,
+                phone_number=shipping_details.phone,
+                country=shipping_details.address.country,
+                eircode=shipping_details.address.postal_code,
+                town_or_city=shipping_details.address.city,
+                street_address1=shipping_details.address.line1,
+                street_address2=shipping_details.address.line2,
+                county=shipping_details.address.state,
+                original_bag=bag,
+                stripe_pid=pid,
+            )
+            for item_id, quantity in json.loads(bag).items():
+                product = Product.objects.get(id=item_id)
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=product,
+                    quantity=quantity,
+                )
+                order_line_item.save()
+        except Exception  as e:
+            if order:
+                order.delete()
+            return HttpResponse(content=f'Webhook received:{event['type']} | Error:{e}',status=500)
+        
 
-        message = f'Webhook received: {event["type"]}'
-        return HttpResponse(
-            content=message, status=200
-            
-        )
 
     def handle_payment_intent_failed(self, event):
         '''
