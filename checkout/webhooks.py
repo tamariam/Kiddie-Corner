eade2e3baf8706d6e7c3226ec8a15c5176b5636a
sopiko abulidze <abulidze2019@gmail.com>
2024-05-16 12:03:48 +0000

    start modification of webhook function,Setup Stripe API key and required imports for webhook function

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,6 +1,13 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+from checkout.webhook_handler import StripeWH_Handler
+
+
 def webhook(request):
     """Listen for webhooks from Stripe"""
-    # Setup
+    # Setup stripe api key 
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
