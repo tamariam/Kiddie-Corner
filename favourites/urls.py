f2bfaf3a929c83c4af4f5c3f00f1b9ddbcc3f41b
sopiko abulidze <abulidze2019@gmail.com>
2024-05-21 14:53:53 +0000

    Implement view for add_to_favourites feature, add view link to product details page, define URL for add_to_favourite

--- favourites/urls.py
+++ favourites/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from.views import favourites
+from.views import favourites, add_to_favourite
 
 urlpatterns = [
     path('', favourites, name='favourites'),
+    path('add_to_favourite<int:product_id>/', add_to_favourite, name='add_to_favourite'),
+    
+
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
+from products.models import Product
+from .models import Favourite
+from django.contrib import messages
 
 # Create your views here.
 
 
 def favourites(request):
     return render(request, 'favourites/favourites.html')
+
+
+def add_to_favourite(request, product_id):
+    '''view to add product in favourites
+    '''
+    product = get_object_or_404(Product, id=product_id)
+    # Check if the product is already in favourites list 
+    if Favourite.objects.filter(user=request.user, product=product).exists():
+        messages.info(request, f'{product.name} is already in your favourites')
+    else:
+        Favourite.objects.create(user=request.user, product=product)
+        messages.success(request, f'{product.name} has b een added to your favourites')
+    return redirect('product_detail', product_id=product_id)
+    
+
+
