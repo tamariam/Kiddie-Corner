b28c0be33d4f4610429880139e6e8c9b9706b182
sopiko abulidze <abulidze2019@gmail.com>
2024-05-21 16:05:17 +0000

    implement remove_favourite view to remove an item from favourites, also define its urls and update favourites template with remove_favourites link but i have an issue   with this code i needd further modifications

--- favourites/urls.py
+++ favourites/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from.views import favourites, add_to_favourite
+from.views import favourites, add_to_favourite,remove_favourite
 
 urlpatterns = [
     path('', favourites, name='favourites'),
-    path('add_to_favourite<int:product_id>/', add_to_favourite, name='add_to_favourite'),
+    path('add_to_favourite/<int:product_id>/', add_to_favourite, name='add_to_favourite'),
+    path('remove_favourite/<int:product_id>/', add_to_favourite, name='remove_favourite'),
+
     
 
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -21,7 +21,20 @@ def add_to_favourite(request, product_id):
     else:
         Favourite.objects.create(user=request.user, product=product)
         messages.success(request, f'{product.name} has been added to your favourites')
-    return redirect('product_detail', product_id=product_id)
+    return redirect('favourites')
     
+
+def remove_favourite(request, product_id):
+    """ up a quantity of the specified product to the shopping bag """
+    product = get_object_or_404(Product, id=product_id)
+    favourites = Favourite.objects.filter(user=request.user, product=product)
+    if favourites.exists():
+        favourites.delete()
+        messages.success(request, f'{product.name} has been removed from your favourites')
+    else:
+        messages.error(request, f'can not find {product.name} in your favourites')
+    return redirect('product_detail', product_id=product_id)
+
+
 
 
