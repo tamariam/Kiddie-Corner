38238762771da122bced66b16eb74fa1c6b6f5a8
sopiko abulidze <abulidze2019@gmail.com>
2024-05-21 15:03:43 +0000

    copy bag.html code to favourites.html for future modifications

--- favourites/views.py
+++ favourites/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
 from .models import Favourite
 from django.contrib import messages
