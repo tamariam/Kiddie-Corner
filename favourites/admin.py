1471ba525556a6bb0aee8e1985a9ed13c29db08f
sopiko abulidze <abulidze2019@gmail.com>
2024-05-22 09:44:38 +0000

    register Favourite model to admin panel

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Favourite
 
 # Register your models here.
+
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    """
+    access the Category fields model in Django Admin panel
+    """
+    list_display = (
+        'user', 'product', 'added_on')
+    search_fields = (
+        'product',)
+
+    readonly_fields = (
+        'added_on',
+    )
+    
+    ordering = ('-added_on',)
