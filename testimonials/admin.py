406cadb1e367ca922bd5f02269f7e3fc38f65922
sopiko abulidze <abulidze2019@gmail.com>
2024-05-27 10:02:38 +0000

    register Testimonial model in admin pane;

--- testimonials/admin.py
+++ testimonials/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Testimonial
 # Register your models here.
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    """
+    access the Testimonial fields model in Django Admin panel
+    """
+    list_display = (
+        'name', 'profession',)
+    search_fields = (
+        'name',)
+
