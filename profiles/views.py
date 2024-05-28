53c618687e94419b99ee502fba25ea1973b5ebc6
sopiko abulidze <abulidze2019@gmail.com>
2024-05-28 21:02:05 +0000

    updayte profile view with POST handler  to handl epost requests

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .forms import UserProfileForm
 from .models import UserProfile
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,10 +9,16 @@ from .models import UserProfile
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+    if request == "POST":
+        form = UserProfileForm(request.POST,instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your Profile Updated')
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     context = {
         'form': form,
+        'orders':orders
 
 
     }
