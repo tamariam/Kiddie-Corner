494a09aae55670ba9f7280ff667c725dfdaac01f
sopiko abulidze <abulidze2019@gmail.com>
2024-05-29 13:52:37 +0000

    modify checkout success view to get the user data if save_info box is checked

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,7 @@ import stripe
 from django.conf import settings
 from products.models import Product
 from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from .models import Order, OrderLineItem
 from django.views.decorators.http import require_POST
 import json
@@ -55,7 +56,7 @@ def checkout(request):
             order = checkout_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
             order_stripe_pid = pid
-            order.original_bag=json.dumps(bag)
+            order.original_bag = json.dumps(bag)
             
             for item_id, quantity in bag.items():
                 try:
@@ -109,11 +110,23 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save-info')
     order = get_object_or_404(Order, order_number=order_number)
     if request.user.is_authenticated:
-        #attach the user sprofile to the order
+        #attach the users profile to the order
         profile = UserProfile.objects.get(user=request.user)
         order.user_profile = profile
         order.save()
-
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance =profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(request, f'Your order processed successfully, a confirmation email will be sent to{order.email}')
     if 'bag' in request.session:
