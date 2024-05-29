3969817109aabeca6ee94086ef4ffd927d00e0d1
sopiko abulidze <abulidze2019@gmail.com>
2024-05-29 13:25:25 +0000

    Create completed_order view to allow users to view details of their previous orders from profile page

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import UserProfileForm
 from .models import UserProfile
 from django.contrib import messages
+from checkout.models import Order
 
 
 # Create your views here.
@@ -24,3 +25,19 @@ def profile(request):
 
     }
     return render(request, 'profiles/profile.html', context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+                            f'''This is a past confirmation
+                            for order number {order_number}. '''
+                            'A confirmation email was sent on the order date.'
+    ))
+
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+    return render(request, 'checkout/checkout_success.html', context)
