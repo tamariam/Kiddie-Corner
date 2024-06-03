0a80262b5e9d4b888b17d59e39c6d2c34a610dc9
sopiko abulidze <abulidze2019@gmail.com>
2024-06-03 13:41:12 +0000

    set debug to false to find out why i have bad request on heroku

--- kiddie_corner/settings.py
+++ kiddie_corner/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-tamariam-kiddiecorner-pmgxf0gq0yq.ws-eu114.gitpod.io', 'https://kiddie-corner-77bf42039400.herokuapp.com/']
 
