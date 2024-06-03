f889dfbf5b4aa7d405c21a7586a886fc90a55866
sopiko abulidze <abulidze2019@gmail.com>
2024-06-03 21:42:15 +0000

    had mistake in heroku link

--- kiddie_corner/settings.py
+++ kiddie_corner/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 
-ALLOWED_HOSTS = ['8000-tamariam-kiddiecorner-pmgxf0gq0yq.ws-eu114.gitpod.io', 'https://kiddie-corner-d9bc0a883cc2.herokuapp.com/']
+ALLOWED_HOSTS = ['8000-tamariam-kiddiecorner-pmgxf0gq0yq.ws-eu114.gitpod.io', 'kiddie-corner-d9bc0a883cc2.herokuapp.com/']
 
 
 # Application definition
