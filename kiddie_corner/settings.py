59093e8192cd261f6f835525cf68e8ef86dfcb06
sopiko abulidze <abulidze2019@gmail.com>
2024-06-03 13:25:10 +0000

    add heroku app to allowed hosts

--- kiddie_corner/settings.py
+++ kiddie_corner/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-tamariam-kiddiecorner-pmgxf0gq0yq.ws-eu114.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-tamariam-kiddiecorner-pmgxf0gq0yq.ws-eu114.gitpod.io', 'https://kiddie-corner-77bf42039400.herokuapp.com/']
 
 
 # Application definition
